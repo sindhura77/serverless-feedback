b277b231c42727d8b1f5fc24ce637b1745961cbe
sindhura77 <45347991+sindhura77@users.noreply.github.com>
2019-11-21 00:46:12 +0530

    Update feedback.js

--- docs/feedback.js
+++ docs/feedback.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://d526c02f.eu-gb.apiconnect.appdomain.cloud/07-feedback';
+const apiUrl = 'https://bd34dc68.eu-gb.apiconnect.appdomain.cloud/sk-feedback';
 const feedback = {
   // retrieve the existing feedback entries
   get() {
